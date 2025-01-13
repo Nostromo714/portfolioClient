47aaf90139571bf9a565cd87cb7e6e067f2c27b4
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2025-01-13 13:16:25 -0500

    added postcss.config and tailwind.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Scan all files in the 'src' folder with these extensions
+    ],
   theme: {
     extend: {},
   },

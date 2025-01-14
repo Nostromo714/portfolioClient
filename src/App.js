4e4721de8c63804b06dc1becc2d0c1c170d55a52
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2025-01-14 18:02:44 -0500

    imported font-awesome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import 'font-awesome/css/font-awesome.min.css';
+
 
 // Import your pages
 import HomePage from './pages/home';  // This will work with default export
@@ -8,17 +10,9 @@ import HomePage from './pages/home';  // This will work with default export
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-          </ul>
-        </nav>
-
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
-      </div>
     </Router>
   );
 }

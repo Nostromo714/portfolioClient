2130b66915f1a23f700a182e78549d1e173b4c5b
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2025-01-30 01:03:07 -0500

    added two more Array slides for displaying diferent projects in the 3 diferent modals

--- src/assets/carouselData.js
+++ src/assets/carouselData.js
@@ -1,16 +1,55 @@
-export const slides = [
+export const storySphere = [
   {
     src: "https://picsum.photos/600/400/?blur",
-    alt: "Image 1 for carousel"
+    alt: "Image 1 for carousel",
+    description: "project 1 This is the first image description, explaining what the image is about.",
   },
   {
     src: "https://picsum.photos/600/400?grayscale",
-    alt: "Image 2 for carousel"
+    alt: "Image 2 for carousel",
+    description: "This is the second.",
+    
   },
   {
     src: "https://picsum.photos/600/400?grayscale",
-    alt: "Image 2 for carousel"
+    alt: "Image 2 for carousel",
+    description: "This is the third.",
   }
 ];
 
+export const projectTwoSlides = [
+  {
+    src: "https://picsum.photos/600/400/?random", // Project 2 Image 1
+    alt: "Image 1 for project 2",
+    description: "Project 2 image 1, illustrating the overall design of the project."
+  },
+  {
+    src: "https://picsum.photos/600/400/?random&grayscale", // Project 2 Image 2
+    alt: "Image 2 for project 2",
+    description: "This is an image of the second feature in Project 2."
+  },
+  {
+    src: "https://picsum.photos/600/400/?random&blur", // Project 2 Image 3
+    alt: "Image 3 for project 2",
+    description: "Here we show another angle or part of the Project 2."
+  }
+];
+  
+export const projectThreeSlides = [
+  {
+    src: "https://picsum.photos/600/400/?random", // Project 3 Image 1
+    alt: "Image 1 for project 3",
+    description: "Project 3 image 1, illustrating the overall design of the project.",
+  },
+  {
+    src: "https://picsum.photos/600/400/?random&grayscale", // Project 3 Image 2
+    alt: "Image 2 for project 3",
+    description: "This is an image of the second feature in Project 2.",
+  },
+  {
+    src: "https://picsum.photos/600/400/?random&blur", // Project 3 Image 3
+    alt: "Image 3 for project 3",
+    description: "Here we show another angle or part of the Project 2.",
+  }
+];
   

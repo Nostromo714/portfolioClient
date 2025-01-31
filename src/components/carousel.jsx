5258562e9c11172f625dbd8c9c911381e6ca61c3
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2025-01-31 11:01:09 -0500

    wrapped img and description in div to create the look for dicription in carosel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -17,42 +17,34 @@ export const Carousel = ({ data, activeIndex, onSlideChange }) => {
     {/* Carousel */}
     return (
         
-        <div className=" relative border-black pb-10 bg-black border-2 p-5 flex flex-col justify-center items-center">
+        <div className=" relative flex flex-col justify-center items-center max-h-screen p-2">
 
-            
         {/* Left Arrow */}
         <BsArrowLeftCircle 
         onClick={handleLeftArrowClick}
         className=" absolute left-6 top-1/2 transform text-gray-400 text-4xl cursor-pointer rounded-full z-10 hover-pointer hover:opacity-50 shadow-lg" />
 
-         {/* Image */}
-         <div className="w-full h-[400px]"> {/* Set a fixed height */}
+         {/* Image + Description Wrapper */}
+         <div className="border-black border-2 max-w-[80wh] max-h-[70vw] flex flex-col justify-center items-center"> 
+
         <img 
         src={data[activeIndex].src} 
         alt={data[activeIndex].alt} 
-        className="w-full object-cover m-2 rounded"/> 
+        className="object-contain max-w-a max-auto-[700px]"
+         /> 
+         
+         {/* Description Below Image */}
+        <div 
+        className="text-white text-lg bg-black w-full text-center p-5 ">
+        <p className=''>{data[activeIndex].description}</p> {/* Displaying description for the current slide */}
+        
+        </div>
         </div>
          {/* Right Arrow */}
         <BsArrowRightCircle 
         onClick={handleRightArrowClick}
         className=" absolute right-6 top-1/2 transform text-gray-400 text-4xl cursor-pointer  z-10 hover:opacity-50 shadow-lg"/>
 
-        {/* Carousel Buttons (for navigation or other purpose) */}
-         <span className=" absolute bottom-0 border flex justify-center space-x-2  p-2 rounded-full shadow-lg ">
-        {data.map((__, idx) => {
-            return (
-        <button 
-        key={idx}
-        onClick={() => onSlideChange(idx)}
-        className={`w-3 h-3 rounded-full bg-gray-600 ${activeIndex === idx ? '' : 'bg-slate-400  shadow-lg'} transition-all`}/>
-            )
-        })}
-    </span>
-     {/* Description Below Carousel */}
-     <div 
-     className="mt-4 text-white text-lg">
-        <p>{data[activeIndex].description}</p> {/* Displaying description for the current slide */}
-    </div>
     </div>
     )
 }
